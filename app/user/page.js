4c0dbdda197f559727efd2e920264acc87d177b1
Manul Thanura <manulkarunarathna@gmail.com>
2023-08-22 10:44:47 +0530

    Update page.js

---
+++ app/user/page.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+// localhost:3000/user/page;
+
+const page = () => {
+    return (
+        <div>
+        <h1>hello user</h1>
+        </div>
+    )
+    }
+
+export default page
