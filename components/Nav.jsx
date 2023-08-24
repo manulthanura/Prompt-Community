dcd3311edf2b3b662b58946411171b4b0e4bb495
Manul Thanura <manulkarunarathna@gmail.com>
2023-08-24 11:43:51 +0530

    Update Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,6 +6,7 @@ import { useState, useEffect } from 'react';
 import {signIn, signOut, useSession, getProviders} from 'next-auth/react'
 
 const Nav = () => {
+  const isUserLoggedIn = true;
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -18,6 +19,19 @@ const Nav = () => {
         />
         <p className="logo_text">GitHub Promt</p>
       </Link>
+      {/* Desktop Navigation */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className='black_btn'>
+              Create Post
+            </Link>
+          </div>
+        ) : (
+          <>
+          </>
+        )}
+      </div>
     </nav>
   )
 }
