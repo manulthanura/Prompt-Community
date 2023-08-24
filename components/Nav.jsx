5cdfcc2ace20f8dc7ab6dc559f970311509d569e
Manul Thanura <manulkarunarathna@gmail.com>
2023-08-24 11:35:08 +0530

    Update Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,7 +8,7 @@ import {signIn, signOut, useSession, getProviders} from 'next-auth/react'
 const Nav = () => {
   return (
     <nav className="flex-between w-full mb-16 pt-3">
-      <link href="/" className="flex gap-2 flex-center">
+      <Link href="/" className="flex gap-2 flex-center">
         <Image
           src="/assets/images/logo.svg"
           alt="Logo"
@@ -16,7 +16,8 @@ const Nav = () => {
           height={30}
           className="object-contain"
         />
-      </link>
+        <p className="logo_text">GitHub Promt</p>
+      </Link>
     </nav>
   )
 }
