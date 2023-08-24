5366a2faa3256e0a89c9948dd5246d003a4715ea
Manul Thanura <manulkarunarathna@gmail.com>
2023-08-24 11:07:37 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,10 @@
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">Welcome to my Next.js app!</h1>
+        <p className="text-2xl">This is a simple Next.js app that I built to learn Next.js.</p>
+    </section>
   )
 }
 
