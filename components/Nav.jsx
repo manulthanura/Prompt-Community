6e5836a4753c65f50b35933021ed4217f3904c72
Manul Thanura <manulkarunarathna@gmail.com>
2023-08-24 11:32:19 +0530

    Update Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,9 +1,23 @@
-import Link from "next/link"
-import Image from "next/image"
+"use client";
+
+import Link from "next/link" ;
+import Image from "next/image" ;
+import { useState, useEffect } from "react";
+import {signIn, signOut, useSession, getProviders} from 'next-auth/react'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav className="flex-between w-full mb-16 pt-3">
+      <link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="Logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+      </link>
+    </nav>
   )
 }
 
