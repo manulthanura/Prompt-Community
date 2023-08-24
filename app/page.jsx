134a998a37b799a585fc4080cd8a7cdfa87f1b8b
Manul Thanura <manulkarunarathna@gmail.com>
2023-08-24 11:14:06 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,13 @@
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
-        <h1 className="head_text text-center">Welcome to my Next.js app!</h1>
-        <p className="text-2xl">This is a simple Next.js app that I built to learn Next.js.</p>
+        <h1 className="head_text text-center">Welcome to my Next.js app!
+        <br className="max-md:hidden"/>
+        <span className="orange_gradient text-center">This is a simple Next.js app that I built to learn Next.js.</span>
+        </h1>
+        <p className="desc text-center">This app is deployed on Vercel and is using the </p>
+
+        {/* Feed */}
     </section>
   )
 }
