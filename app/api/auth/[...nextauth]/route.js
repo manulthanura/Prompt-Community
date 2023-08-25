4c6a0ca0af79ccba4ce0c2f4a4ddf46adc7c91d7
Manul Thanura <manulkarunarathna@gmail.com>
2023-08-25 09:46:00 +0530

    Update route.js

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,22 @@
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: '',
+            clientSecret: '',
+            // clientId: process.env.GOOGLE_CLIENT_ID,
+            // clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
+    ],
+    async session({ session}) {
+        // session.user.id = user.id;
+        // return session;
+    },
+    async signIn({profile}){
+
+    }
+})
+
+export { handler as GET, handler as POST};
