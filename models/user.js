35904e2cbd13dbab1bccdf83b2641c798bedd496
Manul Thanura <manulkarunarathna@gmail.com>
2023-08-25 11:25:14 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 import { Schema, model, models } from "mongoose";
 
-const userSchema = new Schema({
+const UserSchema = new Schema({
     email: {
         type: String,
         unique: [true, "Email already exists"],
@@ -10,5 +10,13 @@ const userSchema = new Schema({
         type: String,
         unique: [true, "Username already exists"],
         required: [true, "Username is required"],
+        match: [/^[a-zA-Z0-9]+$/, "Username can only contain alphanumeric characters"],
+    },
+    image: {
+        type: String,
     },
 });
+
+const User = models.User || model("User", UserSchema);
+
+export default User;
