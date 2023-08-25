e7892c37bc200aee7a0efcab66de75e04e596a08
Manul Thanura <manulkarunarathna@gmail.com>
2023-08-25 09:18:49 +0530

    Update Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,7 +8,7 @@ import {signIn, signOut, useSession, getproviders} from 'next-auth/react'
 const Nav = () => {
   const isUserLoggedIn = true;
   const [providers, setproviders] = useState(null);
-  const [toggleDropdown, settoggleDropdown] = useState(false);
+  const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
     const setproviders = async () => {
@@ -76,8 +76,35 @@ const Nav = () => {
             height={37}
             className="rounded-full"
             alt="Profile" 
-            onClick={() => settoggleDropdown ((prev) => !prev)}
+            onClick={() => setToggleDropdown ((prev) => !prev)}
             />
+            {toggleDropdown && (
+              <div className="dropdown">
+                <Link 
+                  href="/profile" 
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  My Profile
+                </Link>
+                <Link 
+                  href="/create-prompt" 
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Create Promt
+                </Link>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                  className="mt-5 w-full black_btn">
+                  Sign Out
+                </button>
+              </div>
+            )}
           </div>
         ):(
         <>
