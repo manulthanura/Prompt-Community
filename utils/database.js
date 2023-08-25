c8adc238ffa67bfb9653788a8a60e17a307777a8
Manul Thanura <manulkarunarathna@gmail.com>
2023-08-25 10:16:09 +0530

    Update database.js

---
+++ utils/database.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+let isConnected = false; // global variable to cache the connection
+
+export const connectToDB = async () => {
+    mongoose.set('strictQuery', true);
+    is (isConnected) {
+
+    }
+}
