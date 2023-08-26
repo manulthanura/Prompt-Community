7b32d9251ac5c79b70f74c70610fbb360c3c780d
Manul Thanura <manulkarunarathna@gmail.com>
2023-08-26 09:03:47 +0530

    Update Form.jsx

--- components/Form.jsx
+++ components/Form.jsx
@@ -23,17 +23,29 @@ const Form = ({ type, post, setPost, submitting, handleSubmit}) => {
         </label>
         <label>
           <span className="font-satoshi font-semibold text-base text-gray-700">
-            Prompt Tag
+            Tag {``}
             <span className="font-normal">(#product #web)</span>
           </span>
-          <textarea 
-            value={post.prompt}
-            onChange={(e) => setPost({...post, prompt: e.target.value})}
-            placeholder="What is your prompt?"
+          <input
+            value={post.tag}
+            onChange={(e) => setPost({...post, tag: e.target.value})}
+            placeholder="#tag"
             required
-            className="form_textarea"
+            className="form_input"
           />
         </label>
+        <div className="flex-end mx-3 mb-5 gap-4">
+          <Link href="/" className="text-gray-500 text-sm">
+            Cancel
+          </Link>
+          <button
+            type="submit"
+            disabled={submitting}
+            className="px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white"
+          >
+            {submitting ? `${type}...` : type}
+          </button>
+        </div>
       </form>
     </section>
   )
