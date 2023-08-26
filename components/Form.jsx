f10534960f2f76d2cb975985be56173f5004ebb7
Manul Thanura <manulkarunarathna@gmail.com>
2023-08-26 08:47:15 +0530

    Update Form.jsx

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import Link from 'next/link';
 
 const Form = ({ type, post, setPost, submitting, handleSubmit}) => {
   return (
-    <div>Form</div>
+    <section className="w-full max-w-full flex-start flex-col">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{type} Post</span>
+      </h1>
+      <p className="desc text-left max-w-md">
+        {type} a post to the community. Please be kind and respectful.
+      </p>
+
+      <form className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism" onSubmit={handleSubmit}>
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">Your Prompt</span>
+          <textarea 
+          value={post.prompt}
+          onChange={(e) => setPost({...post, prompt: e.target.value})}
+          placeholder="What is your prompt?"
+          required
+          className="form_textarea"
+          />
+        </label>
+      </form>
+    </section>
   )
 }
 
