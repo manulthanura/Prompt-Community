81759c6fca2984bca731f5283f89131737d8e629
Manul Thanura <manulkarunarathna@gmail.com>
2023-08-26 08:24:19 +0530

    Update page.jsx

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+'use client';
+import {useState} from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
+import Form from '@components/Form';
 
 const CreatePrompt = () => {
+    const [submitting, setSubmitting] = useState(false);
+    const [post, setPost] = useState({
+        prompt: '',
+        tags: '',
+    });
+
+    const createPost = async (e) => {
+
+    }
   return (
-    <div>CreatePrompt</div>
+    <Form
+        type="Create"
+        post={post}
+        setPost={setPost}
+        submitting={submitting}
+        handleSubmit={createPost}
+    />
   )
 }
 
