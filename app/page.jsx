a9be1632dbdc18680830fe08d75002bf10ae1d56
Manul Thanura <manulkarunarathna@gmail.com>
2023-08-27 11:37:58 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -4,9 +4,9 @@ import Feed from "@components/Feed";
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
-        <h1 className="head_text text-center">Welcome to my Next.js app!
+        <h1 className="head_text text-center">Welcome to the AI Prompt Ideas Community!
         <br className="max-md:hidden"/>
-        <span className="orange_gradient text-center">This is a simple Next.js app that I built to learn Next.js.</span>
+        <span className="orange_gradient text-center">The place where people can share and discuss AI prompt ideas</span>
         </h1>
         <p className="desc text-center">This app is deployed on Vercel and is using the </p>
 
