aab1d110f3cdb2f4e0f20f412c1a4435bb858e87
Manul Thanura <manulkarunarathna@gmail.com>
2023-08-28 10:11:14 +0530

    Update Form.jsx

--- components/Form.jsx
+++ components/Form.jsx
@@ -3,13 +3,13 @@ import Link from 'next/link';
 const Form = ({ type, post, setPost, submitting, handleSubmit}) => {
   return (
     <section className="w-full max-w-full">
-      <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+      <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-10 lg:px-6">
           <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
             <h1 className="head_text text-left">
               <span className="green_gradient">{type} Post</span>
             </h1>
             <p className="desc text-left max-w-md">
-              {type} a post to the community. Please be kind and respectful.
+              Share your creativity with the world. {type} a prompt that inspires others
             </p>
 
             <form className="my-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism" onSubmit={handleSubmit}>
@@ -50,8 +50,8 @@ const Form = ({ type, post, setPost, submitting, handleSubmit}) => {
               </div>
             </form> 
           </div>
-          <div class="grid grid-cols-2 gap-4 mt-8">
-              <img class="w-full rounded-lg" src="" alt="office content 1"/>
+          <div class="grid grid-cols-1 gap-4 mt-8">
+              <img class="w-full rounded-lg" src="/assets/images/cp.jpg" alt="office content 1"/>
           </div>
       </div>
     </section>
